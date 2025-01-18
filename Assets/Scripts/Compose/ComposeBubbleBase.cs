348da1175d5b03c65e1983a2473e08fe7927c443
Chlamydomona5 <2227258508@qq.com>
2025-01-18 13:44:57 +0800

    1

--- Assets/Scripts/Compose/ComposeBubbleBase.cs
+++ Assets/Scripts/Compose/ComposeBubbleBase.cs
@@ -1,16 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class ComposeBubbleBase : MonoBehaviour
 {
     [SerializeField] private BubbleData data;
+
+    [SerializeField] private Vector2 releaseForce;
     
     private bool _onMouseControl;
     private Rigidbody2D _rigidbody2D;
     private Collider2D _collider2D;
+
+    [SerializeField, ReadOnly] private float size;
 
     public void Init(BubbleData data)
     {
@@ -37,5 +42,18 @@ public class ComposeBubbleBase : MonoBehaviour
     private void OnMouseUp()
     {
         _onMouseControl = false;
+    }
+
+    public void Blow(float delta)
+    {
+        size += delta;
+        transform.localScale = Vector3.one * size;
+        transform.position += Vector3.up * delta / 2f;
+    }
+
+    public void Release()
+    {
+        _rigidbody2D.bodyType = RigidbodyType2D.Dynamic;
+        _rigidbody2D.AddForce(releaseForce);
     }
 }
--- Assets/Scripts/Compose/ComposeController.cs
+++ Assets/Scripts/Compose/ComposeController.cs
@@ -1,47 +1,84 @@
 ﻿using System;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class ComposeController : Singleton<ComposeController>
 {
-    public float crabSpeed = 1f;
+    [SerializeField] private float crabSpeed = 1f;
+    [SerializeField] private float consumeLiquidSpeed = 1f;
+    [SerializeField] private float minimumLiquid = 0.1f;
+    [SerializeField] private float blowSpeed = 1f;
+    [SerializeField] private float minimumBlowVolume = 0.1f;
+    [SerializeField] private float bubbleInitialYOffset = 1f;
 
     [SerializeField] private GameObject crab;
     [SerializeField] private GameObject craw;
 
     [SerializeField] private ComposeBubbleBase bubblePrefab;
-    [SerializeField] private ComposeBubbleBase currentInitingBubble;
-    [SerializeField] private LiquidBottle currentTouchingLiquidBottle;
+    [SerializeField, ReadOnly] private ComposeBubbleBase currentInitingBubble; 
+    public LiquidBottle currentTouchingLiquidBottle;
     
     private void Update()
     {
         var mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mouseWorldPosition = new Vector3(mouseWorldPosition.x, mouseWorldPosition.y, 0);
-        // Crab Move
-        if(Input.GetKey(KeyCode.A)) crab.transform.position += Vector3.left * crabSpeed * Time.deltaTime;
-        if(Input.GetKey(KeyCode.D)) crab.transform.position += Vector3.right * crabSpeed * Time.deltaTime;
+
         // Craw Move
         craw.transform.position = mouseWorldPosition;
-        // Click Handler
-        /*if (Input.GetMouseButtonDown(0))
+        // GenerateBubble
+        if (Input.GetKey(KeyCode.Space))
         {
-            //RayCast Bubble at mouse position
-            var hit = Physics2D.Raycast(mouseWorldPosition, Vector2.up, .1f, LayerMask.GetMask("Bubble"));
-            if (hit.collider)
+            // If there is no bubble initing, and there is a liquid bottle touching
+            if (!currentInitingBubble && currentTouchingLiquidBottle)
             {
-                hit.collider.GetComponent<ComposeBubbleBase>().OnMouseControl(true);
+                TryGenerateBubbleAt(currentTouchingLiquidBottle.Data, currentTouchingLiquidBottle.transform.position);
             }
-        }*/
+            // If there is already a bubble initing
+            if (currentInitingBubble)
+            {
+                BlowCurrentBubble();
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (currentInitingBubble)
+            {
+                ReleaseCurrentBubble();
+            }
+        }
+        else
+        {
+            // Crab Move
+            if(Input.GetKey(KeyCode.A)) crab.transform.position += Vector3.left * crabSpeed * Time.deltaTime;
+            if(Input.GetKey(KeyCode.D)) crab.transform.position += Vector3.right * crabSpeed * Time.deltaTime;
+        }
+    }
+    
+    public void TryGenerateBubbleAt(BubbleData data, Vector2 pos)
+    {
+        if (currentTouchingLiquidBottle.TryConsumeLiquid(minimumLiquid))
+        {
+            var bubble = Instantiate(bubblePrefab, pos + Vector2.up * bubbleInitialYOffset, Quaternion.identity);
+            bubble.Init(data);
+            currentInitingBubble = bubble;
+            currentInitingBubble.Blow(minimumBlowVolume);
+        }
     }
 
-    public void GenerateBubbleAt(BubbleData data, Vector2 pos)
+    public bool BlowCurrentBubble()
     {
-        var bubble = Instantiate(bubblePrefab, pos, Quaternion.identity);
-        bubble.Init(data);
-        currentInitingBubble = bubble;
+        var delta = Time.deltaTime;
+        if (currentTouchingLiquidBottle.TryConsumeLiquid(delta * consumeLiquidSpeed))
+        {
+            currentInitingBubble.Blow(delta * blowSpeed);
+            return true;
+        }
+        return false;
     }
-
-    public void BlowCurrentBubble()
+    
+    private void ReleaseCurrentBubble()
     {
-        
+        currentInitingBubble.Release();
+        currentInitingBubble = null;
     }
 }
