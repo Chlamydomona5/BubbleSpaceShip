85a1e75d6b9d94b34cb1cc51c82f0fc01dded115
Chlamydomona5 <2227258508@qq.com>
2025-01-18 17:27:47 +0800

    1

--- Assets/Scripts/Compose/BubbleData.cs
+++ Assets/Scripts/Compose/BubbleData.cs
@@ -3,5 +3,26 @@
 [CreateAssetMenu(fileName = "BubbleData", menuName = "SO/BubbleData")]
 public class BubbleData : ScriptableObject
 {
-    
+    public ExplodeEffect ExplodeEffect;
+}
+
+public abstract class ExplodeEffect
+{
+    public abstract void Effect(BubbleShip ship, Vector2 effectPos, float amount);
+}
+
+public class PushEffect : ExplodeEffect
+{
+    public override void Effect(BubbleShip ship, Vector2 effectPos, float amount)
+    {
+        
+    }
+}
+
+public class RotateEffect : ExplodeEffect
+{
+    public override void Effect(BubbleShip ship, Vector2 effectPos, float amount)
+    {
+        
+    }
 }
