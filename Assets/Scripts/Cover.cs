4ac7184920c77e3ede45046c6b0c13783f322b6b
Chlamydomona5 <2227258508@qq.com>
2025-01-19 15:14:25 +0800

    4111

---
+++ Assets/Scripts/Cover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Cover : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] private List<Image> images;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        StartCoroutine(Explode());
+    }
+
+    private IEnumerator Explode()
+    {
+        yield return new WaitForSeconds(0.1f);
+        images[0].gameObject.SetActive(false);
+        images[1].gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        images[1].gameObject.SetActive(false);
+        images[2].gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        images[2].gameObject.SetActive(false);
+        images[3].gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        images[3].gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.1f);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+}
